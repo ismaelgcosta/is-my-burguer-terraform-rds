5ad96d31601bfcd0719054aa2385c299d7281611
Ismael Costa <1953589+ismaelgcosta@users.noreply.github.com>
2024-02-20 01:45:02 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,18 @@ provider "aws" {
   region = "us-east-1"
 }
 
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.11.5"
+
+  name                 = "ismyburguer"
+  cidr                 = "10.0.0.0/16"
+  azs                  = data.aws_availability_zones.zones.names
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+
 resource "aws_security_group" "postgres" {
   name        = "postgres-security-group"
   description = "Security group for Postgres database"

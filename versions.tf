a5f99178bf6db4b8058a1fe32e68089a16cd0795
Ismael Costa <1953589+ismaelgcosta@users.noreply.github.com>
2024-02-20 00:41:08 -0300

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -8,13 +8,12 @@ terraform {
     }
   }
 
-  backend "remote" {
-    # The name of your Terraform Cloud organization.
+  cloud {
     organization = "ismaelcosta1000"
 
-    # The name of the Terraform Cloud workspace to store Terraform state files in.
     workspaces {
       name = "postgres-is-my-burguer"
     }
   }
+
 }

873b3564f5fcf53aa34275264105767ef91693aa
Ismael Costa <1953589+ismaelgcosta@users.noreply.github.com>
2024-02-20 00:28:04 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_version = ">= 1.5"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.37"
-    }
-  }
-}
-
 provider "aws" {
   region = "us-east-1"
 }

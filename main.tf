fe58eedbc5b590bb4896d86a5e12b8a68f41f602
Ismael Costa <1953589+ismaelgcosta@users.noreply.github.com>
2024-02-20 00:12:27 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,191 +1,33 @@
 provider "aws" {
-  region = local.region
+  region = "us-east-1"
 }
 
-data "aws_caller_identity" "current" {}
-data "aws_availability_zones" "available" {}
-
-locals {
-  name    = "complete-postgresql"
-  region  = "us-east-1"
-  region2 = "us-east-2"
-
-  vpc_cidr = "10.0.0.0/16"
-  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
-
-  tags = {
-    Name       = local.name
-    Example    = local.name
-    Repository = "https://github.com/terraform-aws-modules/terraform-aws-rds"
-  }
-}
-
-################################################################################
-# RDS Module
-################################################################################
-
-module "db" {
-  source = "../../"
-
-  identifier = local.name
-
-  # All available versions: https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_PostgreSQL.html#PostgreSQL.Concepts
+resource "aws_db_instance" "this" {
+  allocated_storage    = 10
   engine               = "postgres"
-  engine_version       = "14"
-  family               = "postgres14" # DB parameter group
-  major_engine_version = "14"         # DB option group
-  instance_class       = "db.t3.micro"
-
-  allocated_storage     = 20
-  max_allocated_storage = 100
-
-  # NOTE: Do NOT use 'user' as the value for 'username' as it throws:
-  # "Error creating DB Instance: InvalidParameterValue: MasterUsername
-  # user cannot be used as it is a reserved word used by the engine"
-  db_name  = "ismyburguer"
-  username = "ismyburguer"
-  port     = 5433
-
-  # setting manage_master_user_password_rotation to false after it
-  # has been set to true previously disables automatic rotation
-  manage_master_user_password_rotation              = true
-  master_user_password_rotate_immediately           = false
-  master_user_password_rotation_schedule_expression = "rate(15 days)"
-
-  multi_az               = true
-  db_subnet_group_name   = module.vpc.database_subnet_group
-  vpc_security_group_ids = [module.security_group.security_group_id]
-
-  maintenance_window              = "Mon:00:00-Mon:03:00"
-  backup_window                   = "03:00-06:00"
-  enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
-  create_cloudwatch_log_group     = true
-
-  backup_retention_period = 1
-  skip_final_snapshot     = true
-  deletion_protection     = false
-
-  performance_insights_enabled          = true
-  performance_insights_retention_period = 7
-  create_monitoring_role                = true
-  monitoring_interval                   = 60
-  monitoring_role_name                  = "ismyburguer-role"
-  monitoring_role_use_name_prefix       = true
-  monitoring_role_description           = "Description for monitoring role"
-
-  parameters = [
-    {
-      name  = "autovacuum"
-      value = 1
-    },
-    {
-      name  = "client_encoding"
-      value = "utf8"
-    }
-  ]
-
-  tags = local.tags
-  db_option_group_tags = {
-    "Sensitive" = "low"
-  }
-  db_parameter_group_tags = {
-    "Sensitive" = "low"
-  }
+  engine_version       = "12.4"
+  instance_class       = "db.t2.micro"
+  name                 = "myEndpoint"
+  username             = "${env.DB_USERNAME}"
+  password             = "${env.DB_PASSWORD}"
+  parameter_group_name = "default.postgres12"
+  skip_final_snapshot  = true
 }
 
-module "db_default" {
-  source = "../../"
-
-  identifier                     = "${local.name}-default"
-  instance_use_identifier_prefix = true
-
-  create_db_option_group    = false
-  create_db_parameter_group = false
-
-  # All available versions: https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_PostgreSQL.html#PostgreSQL.Concepts
-  engine               = "postgres"
-  engine_version       = "14"
-  family               = "postgres14" # DB parameter group
-  major_engine_version = "14"         # DB option group
-  instance_class       = "db.t3.micro"
-
-  allocated_storage = 20
-
-  # NOTE: Do NOT use 'user' as the value for 'username' as it throws:
-  # "Error creating DB Instance: InvalidParameterValue: MasterUsername
-  # user cannot be used as it is a reserved word used by the engine"
-  db_name  = "ismyburguer"
-  username = "ismyburguer"
-  port     = 5433
-
-  db_subnet_group_name   = module.vpc.database_subnet_group
-  vpc_security_group_ids = [module.security_group.security_group_id]
-
-  maintenance_window      = "Mon:00:00-Mon:03:00"
-  backup_window           = "03:00-06:00"
-  backup_retention_period = 0
-
-  tags = local.tags
+provider "postgresql" {
+  host            = aws_db_instance.this.address
+  port            = 5432
+  username        = aws_db_instance.this.username
+  password        = aws_db_instance.this.password
+  sslmode         = "require"
+  connect_timeout = 15
 }
 
-module "db_disabled" {
-  source = "../../"
-
-  identifier = "${local.name}-disabled"
-
-  create_db_instance        = false
-  create_db_parameter_group = false
-  create_db_option_group    = false
+resource "postgresql-ismyburguer" "ismyburguer" {
+  name = "ismyburguer"
 }
 
-################################################################################
-# RDS Automated Backups Replication Module
-################################################################################
-
-provider "aws" {
-  alias  = "region2"
-  region = local.region2
-}
-
-################################################################################
-# Supporting Resources
-################################################################################
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 5.0"
-
-  name = local.name
-  cidr = local.vpc_cidr
-
-  azs              = local.azs
-  public_subnets   = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
-  private_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 3)]
-  database_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 6)]
-
-  create_database_subnet_group = true
-
-  tags = local.tags
-}
-
-module "security_group" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "~> 5.0"
-
-  name        = local.name
-  description = "Complete PostgreSQL example security group"
-  vpc_id      = module.vpc.vpc_id
-
-  # ingress
-  ingress_with_cidr_blocks = [
-    {
-      from_port   = 5433
-      to_port     = 5432
-      protocol    = "tcp"
-      description = "PostgreSQL access from within VPC"
-      cidr_blocks = module.vpc.vpc_cidr_block
-    },
-  ]
-
-  tags = local.tags
+output "rds_address" {
+  description = "RDS address"
+  value       = aws_db_instance.this.address
 }

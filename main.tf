7274c7a8f13dfeb10363042a1f5d09a40f8dfe1b
Ismael Costa <1953589+ismaelgcosta@users.noreply.github.com>
2024-02-20 00:22:25 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,15 +39,6 @@ resource "aws_db_instance" "this" {
   skip_final_snapshot    = true
 }
 
-provider "postgresql" {
-  host            = aws_db_instance.this.address
-  port            = 5433
-  username        = aws_db_instance.this.username
-  password        = aws_db_instance.this.password
-  sslmode         = "require"
-  connect_timeout = 15
-}
-
 output "rds_address" {
   description = "RDS address"
   value       = aws_db_instance.this.address

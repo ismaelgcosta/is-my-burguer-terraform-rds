16127732ff1d8181e086e2af1b96c868c6ecd484
Ismael Costa <1953589+ismaelgcosta@users.noreply.github.com>
2024-02-20 23:23:32 -0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,9 @@
+output "cluster_endpoint" {
+  description = "The endpoint for the RDS cluster"
+  value = aws_rds_cluster.cluster.endpoint
+}
 
+output "cluster_read_endpoint" {
+  description = "The read endpoint for the RDS cluster"
+  value = aws_rds_cluster.cluster.reader_endpoint
+}

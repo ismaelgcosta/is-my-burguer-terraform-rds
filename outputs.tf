e5ccf9f8b18a8f15e64aa5a551548dd3cf54920a
Ismael G Costa <notmymail@notmymail.com>
2024-03-15 00:22:54 -0300

    adicionando novos outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,14 @@ output "database_endpoint" {
   description = "The endpoint for the RDS cluster"
   value = aws_db_instance.is-my-burguer.endpoint
 }
+
+output "database_endpoint_host" {
+  description = "The address for the RDS cluster"
+  value = aws_db_instance.is-my-burguer.address
+}
+
+output "database_endpoint_port" {
+  description = "The port for the RDS cluster"
+  value = aws_db_instance.is-my-burguer.port
+}
+

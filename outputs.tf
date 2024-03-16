78f817ade9761940eccbf1ab408598a1235cb22d
Ismael G Costa <notmymail@notmymail.com>
2024-03-16 18:44:36 -0300

    fix

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,18 @@
 output "database_endpoint" {
   description = "The endpoint for the RDS cluster"
   value = aws_db_instance.is-my-burguer.endpoint
+  sensitive = true
 }
 
 output "database_endpoint_host" {
   description = "The address for the RDS cluster"
   value = aws_db_instance.is-my-burguer.address
+  sensitive = true
 }
 
 output "database_endpoint_port" {
   description = "The port for the RDS cluster"
   value = aws_db_instance.is-my-burguer.port
+  sensitive = true
 }
 
